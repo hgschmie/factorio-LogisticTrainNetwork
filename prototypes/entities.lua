d8679d8c7468f189058b5bd321ac35d7047b62f4
Henning P. Schmiedehausen <henning@schmiedehausen.org>
2025-01-29 22:04:41 -0800

    remove old low-res entities

--- prototypes/entities.lua
+++ prototypes/entities.lua
@@ -42,42 +42,24 @@ ltn_stop_out.collision_mask = { layers = { rail = true } }                     -
 ltn_stop_out.item_slot_count = 50
 ltn_stop_out.sprites = make_4way_animation_from_spritesheet
     {
-        layers =
-        {
+        layers = {
             {
-                filename = '__LogisticTrainNetwork__/graphics/entity/output.png',
-                width = 58,
-                height = 52,
-                frame_count = 1,
-                shift = util.by_pixel(0, 5),
-                hr_version =
-                {
-                    scale = 0.5,
-                    filename = '__LogisticTrainNetwork__/graphics/entity/hr-output.png',
+                scale = 0.5,
+                    filename = '__LogisticTrainNetwork__/graphics/entity/output.png',
                     width = 114,
                     height = 102,
                     frame_count = 1,
                     shift = util.by_pixel(0, 5),
-                },
             },
             {
-                filename = '__base__/graphics/entity/combinator/constant-combinator-shadow.png',
-                width = 50,
-                height = 34,
-                frame_count = 1,
-                shift = util.by_pixel(9, 6),
-                draw_as_shadow = true,
-                hr_version =
-                {
-                    scale = 0.5,
-                    filename = '__base__/graphics/entity/combinator/hr-constant-combinator-shadow.png',
+                scale = 0.5,
+                    filename = '__base__/graphics/entity/combinator/constant-combinator-shadow.png',
                     width = 98,
                     height = 66,
                     frame_count = 1,
                     shift = util.by_pixel(8.5, 5.5),
                     draw_as_shadow = true,
                 },
-            },
         },
     }
 
