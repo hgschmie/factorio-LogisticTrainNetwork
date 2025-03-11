08fb558925dc9222e6912202b8bcdcbcca67c2ac
Henning P. Schmiedehausen <henning@schmiedehausen.org>
2025-03-10 21:51:52 -0700

    Hide LTN internal entities (stop lamp + combinators) from Factoriopedia

--- prototypes/entities.lua
+++ prototypes/entities.lua
@@ -6,183 +6,116 @@
 
 local collision_mask_util = require('collision-mask-util')
 local data_util = require('prototypes.data-util')
+local meld = require('meld')
+local util = require('util')
 
 local train_stop = data.raw['train-stop']['train-stop']
-train_stop.fast_replaceable_group = train_stop.fast_replaceable_group or 'train-stop'
-train_stop.next_upgrade = 'logistic-train-stop'
+
+local train_stop_update = {
+    fast_replaceable_group = train_stop.fast_replaceable_group or 'train-stop',
+    next_upgrade = 'logistic-train-stop'
+}
+
+data.raw['train-stop']['train-stop'] = meld(train_stop, train_stop_update)
 
 local ltn_stop = data_util.copy_prototype(train_stop, 'logistic-train-stop')
-ltn_stop.icon = '__LogisticTrainNetwork__/graphics/icons/train-stop.png'
-ltn_stop.icon_size = 64
-ltn_stop.next_upgrade = nil
-ltn_stop.selection_box = { { -0.6, -0.6 }, { 0.6, 0.6 } }
--- ltn_stop.collision_box = {{-0.5, -0.1}, {0.5, 0.4}}
+
+local ltn_stop_update = {
+    icon = '__LogisticTrainNetwork__/graphics/icons/train-stop.png',
+    icon_size = 64,
+    next_upgrade = meld.delete(),
+    selection_box = { { -0.6, -0.6 }, { 0.6, 0.6 } },
+    -- collision_box = {{-0.5, -0.1}, {0.5, 0.4}},
+}
+
+ltn_stop = meld(ltn_stop, ltn_stop_update)
 
 local ltn_stop_in = data_util.copy_prototype(data.raw['lamp']['small-lamp'], 'logistic-train-stop-input')
-ltn_stop_in.icon = '__LogisticTrainNetwork__/graphics/icons/train-stop.png'
-ltn_stop_in.icon_size = 64
-ltn_stop_in.next_upgrade = nil
-ltn_stop_in.minable = nil
-ltn_stop_in.selection_box = { { -0.5, -0.5 }, { 0.5, 0.5 } }
-ltn_stop_in.selection_priority = (ltn_stop_in.selection_priority or 50) + 10 -- increase priority to default + 10
-ltn_stop_in.collision_box = { { -0.15, -0.15 }, { 0.15, 0.15 } }
-ltn_stop_in.collision_mask = { layers = { rail = true } }                    -- collide only with rail entities
-ltn_stop_in.energy_usage_per_tick = '10W'
-ltn_stop_in.light = { intensity = 1, size = 6 }
-ltn_stop_in.energy_source = { type = 'void' }
 
+local ltn_stop_in_update = {
+    icon = '__LogisticTrainNetwork__/graphics/icons/train-stop.png',
+    icon_size = 64,
+    next_upgrade = meld.delete(),
+    minable = meld.delete(),
+    selection_box = { { -0.5, -0.5 }, { 0.5, 0.5 } },
+    selection_priority = (ltn_stop_in.selection_priority or 50) + 10, -- increase priority to default + 10
+    collision_box = { { -0.15, -0.15 }, { 0.15, 0.15 } },
+    collision_mask = { layers = { rail = true } },                    -- collide only with rail entities
+    energy_usage_per_tick = '10W',
+    light = { intensity = 1, size = 6 },
+    energy_source = { type = 'void' },
+    hidden_in_factoriopedia = true,
+}
+
+ltn_stop_in = meld(ltn_stop_in, ltn_stop_in_update)
+
+---@type data.ConstantCombinatorPrototype
 local ltn_stop_out = data_util.copy_prototype(data.raw['constant-combinator']['constant-combinator'], 'logistic-train-stop-output')
-ltn_stop_out.icon = '__LogisticTrainNetwork__/graphics/icons/output.png'
-ltn_stop_out.icon_size = 64
-ltn_stop_out.next_upgrade = nil
-ltn_stop_out.minable = nil
-ltn_stop_out.selection_box = { { -0.5, -0.5 }, { 0.5, 0.5 } }
-ltn_stop_out.selection_priority = (ltn_stop_out.selection_priority or 50) + 10 -- increase priority to default + 10
-ltn_stop_out.collision_box = { { -0.15, -0.15 }, { 0.15, 0.15 } }
-ltn_stop_out.collision_mask = { layers = { rail = true } }                     -- collide only with rail entities
-ltn_stop_out.item_slot_count = 50
----@diagnostic disable-next-line: undefined-global
-ltn_stop_out.sprites = make_4way_animation_from_spritesheet
-    {
+
+local ltn_stop_out_update = {
+    icon = '__LogisticTrainNetwork__/graphics/icons/output.png',
+    icon_size = 64,
+    next_upgrade = meld.delete(),
+    minable = meld.delete(),
+    selection_box = { { -0.5, -0.5 }, { 0.5, 0.5 } },
+    selection_priority = (ltn_stop_out.selection_priority or 50) + 10, -- increase priority to default + 10
+    collision_box = { { -0.15, -0.15 }, { 0.15, 0.15 } },
+    collision_mask = { layers = { rail = true } },                     -- collide only with rail entities
+    hidden_in_factoriopedia = true,
+    ---@diagnostic disable-next-line: undefined-global
+    sprites = make_4way_animation_from_spritesheet {
         layers = {
             {
                 scale = 0.5,
-                    filename = '__LogisticTrainNetwork__/graphics/entity/output.png',
-                    width = 114,
-                    height = 102,
-                    frame_count = 1,
-                    shift = util.by_pixel(0, 5),
+                filename = '__LogisticTrainNetwork__/graphics/entity/output.png',
+                width = 114,
+                height = 102,
+                frame_count = 1,
+                shift = util.by_pixel(0, 5),
             },
             {
                 scale = 0.5,
-                    filename = '__base__/graphics/entity/combinator/constant-combinator-shadow.png',
-                    width = 98,
-                    height = 66,
-                    frame_count = 1,
-                    shift = util.by_pixel(8.5, 5.5),
-                    draw_as_shadow = true,
-                },
+                filename = '__base__/graphics/entity/combinator/constant-combinator-shadow.png',
+                width = 98,
+                height = 66,
+                frame_count = 1,
+                shift = util.by_pixel(8.5, 5.5),
+                draw_as_shadow = true,
+            },
         },
-    }
-
-local control_connection_points = {
-    red = util.by_pixel(-3, -7),
-    green = util.by_pixel(-1, 0)
+    },
 }
 
+ltn_stop_out = meld(ltn_stop_out, ltn_stop_out_update)
+
+---@type data.ConstantCombinatorPrototype
 local ltn_lamp_control = data_util.copy_prototype(data.raw['constant-combinator']['constant-combinator'], 'logistic-train-stop-lamp-control')
-ltn_lamp_control.icon = '__LogisticTrainNetwork__/graphics/icons/empty.png'
-ltn_lamp_control.icon_size = 32
-ltn_lamp_control.next_upgrade = nil
-ltn_lamp_control.minable = nil
-ltn_lamp_control.selection_box = { { -0.0, -0.0 }, { 0.0, 0.0 } }
-ltn_lamp_control.collision_box = { { -0.0, -0.0 }, { 0.0, 0.0 } }
-ltn_lamp_control.collision_mask = collision_mask_util.new_mask() -- disable collision
-ltn_lamp_control.item_slot_count = 50
-ltn_lamp_control.flags = { 'not-blueprintable', 'not-deconstructable', 'placeable-off-grid' }
-ltn_lamp_control.sprites =
-{
-    north =
-    {
-        filename = '__LogisticTrainNetwork__/graphics/icons/empty.png',
-        x = 0,
-        y = 0,
-        width = 1,
-        height = 1,
-        frame_count = 1,
-        shift = { 0, 0 },
+
+local ltn_lamp_control_update = {
+    -- icon = '__LogisticTrainNetwork__/graphics/icons/empty.png',
+    -- icon_size = 32,
+    next_upgrade = meld.delete(),
+    minable = meld.delete(),
+    selection_box = meld.delete(),
+    collision_box = meld.delete(),
+    collision_mask = collision_mask_util.new_mask(), -- disable collision
+    flags = {
+        'placeable-off-grid', 'not-repairable', 'not-on-map', 'not-deconstructable', 'not-blueprintable',
+        'hide-alt-info', 'not-flammable', 'not-upgradable', 'not-in-kill-statistics', 'not-in-made-in',
     },
-    east =
-    {
-        filename = '__LogisticTrainNetwork__/graphics/icons/empty.png',
-        x = 0,
-        y = 0,
-        width = 1,
-        height = 1,
-        frame_count = 1,
-        shift = { 0, 0 },
-    },
-    south =
-    {
-        filename = '__LogisticTrainNetwork__/graphics/icons/empty.png',
-        x = 0,
-        y = 0,
-        width = 1,
-        height = 1,
-        frame_count = 1,
-        shift = { 0, 0 },
-    },
-    west =
-    {
-        filename = '__LogisticTrainNetwork__/graphics/icons/empty.png',
-        x = 0,
-        y = 0,
-        width = 1,
-        height = 1,
-        frame_count = 1,
-        shift = { 0, 0 },
-    }
-}
-ltn_lamp_control.activity_led_sprites =
-{
-    north =
-    {
-        filename = '__LogisticTrainNetwork__/graphics/icons/empty.png',
-        width = 1,
-        height = 1,
-        frame_count = 1,
-        shift = { 0.0, 0.0 },
-    },
-    east =
-    {
-        filename = '__LogisticTrainNetwork__/graphics/icons/empty.png',
-        width = 1,
-        height = 1,
-        frame_count = 1,
-        shift = { 0.0, 0.0 },
-    },
-    south =
-    {
-        filename = '__LogisticTrainNetwork__/graphics/icons/empty.png',
-        width = 1,
-        height = 1,
-        frame_count = 1,
-        shift = { 0.0, 0.0 },
-    },
-    west =
-    {
-        filename = '__LogisticTrainNetwork__/graphics/icons/empty.png',
-        width = 1,
-        height = 1,
-        frame_count = 1,
-        shift = { 0.0, 0.0 },
-    }
-}
-ltn_lamp_control.activity_led_light =
-{
-    intensity = 0.0,
-    size = 0,
-}
-ltn_lamp_control.circuit_wire_connection_points =
-{
-    {
-        shadow = control_connection_points,
-        wire = control_connection_points
-    },
-    {
-        shadow = control_connection_points,
-        wire = control_connection_points
-    },
-    {
-        shadow = control_connection_points,
-        wire = control_connection_points
-    },
-    {
-        shadow = control_connection_points,
-        wire = control_connection_points
-    },
+    hidden = true,
+    hidden_in_factoriopedia = true,
+    sprites = util.empty_sprite(),
+    activity_led_sprites = util.empty_sprite(),
+    activity_led_light_offsets = { { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 } },
+    activity_led_light = meld.delete(),
+    draw_circuit_wires = false,
+    draw_copper_wires = false,
+    allow_copy_paste = false,
+    selectable_in_game = false,
 }
 
+ltn_lamp_control = meld(ltn_lamp_control, ltn_lamp_control_update)
 
 data:extend {
     ltn_stop,
@@ -194,15 +127,25 @@ data:extend {
 -- support for cargo ship ports
 if mods['cargo-ships'] then
     local port = data.raw['train-stop']['port']
-    port.fast_replaceable_group = port.fast_replaceable_group or 'port'
-    port.next_upgrade = 'ltn-port'
+
+    local port_update = {
+        fast_replaceable_group = port.fast_replaceable_group or 'port',
+        next_upgrade = 'ltn-port',
+    }
+
+    data.raw['train-stop']['port'] = meld(port, port_update)
 
     ltn_port = data_util.copy_prototype(port, 'ltn-port')
-    ltn_port.icon = '__LogisticTrainNetwork__/graphics/icons/port.png'
-    ltn_port.icon_size = 64
-    ltn_port.next_upgrade = nil
-    ltn_port.selection_box = { { -0.01, -0.6 }, { 1.9, 0.6 } }
-    -- ltn_port.collision_box = {{-0.01, -0.1}, {1.9, 0.4}}
+
+    local ltn_port_update = {
+        icon = '__LogisticTrainNetwork__/graphics/icons/port.png',
+        icon_size = 64,
+        next_upgrade = meld.delete(),
+        selection_box = { { -0.01, -0.6 }, { 1.9, 0.6 } },
+        -- collision_box = {{-0.01, -0.1}, {1.9, 0.4}},
+    }
+
+    ltn_port = meld(ltn_port, ltn_port_update)
 
     data:extend {
         ltn_port
