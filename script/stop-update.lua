hgschmie/factorio-LogisticTrainNetwork

commit cc51872a823ca81b4d3eace34245839df2f9ef7b

remove any limit on LTN train stops

script/stop-update.lua
@@ -119,18 +119,30 @@ function UpdateStop(stopID, stop)
         return
     end
 
-    -- skip deactivated stops
-    local stopCB = stop.entity.get_control_behavior() --[[@as LuaTrainStopControlBehavior ]]
-    if stopCB and stopCB.disabled then
-        stop.error_code = 1
-        tools.reduceAvailableCapacity(stop.parked_train_id)
+    -- also fix up the stop entity itself, in case someone meddled with it
+    stop.entity.trains_limit = nil
 
-        setLamp(stop, ErrorCodes[stop.error_code], 2)
+    local trainstop_control = stop.entity.get_control_behavior() --[[@as LuaTrainStopControlBehavior? ]]
+    if trainstop_control then
+        trainstop_control.send_to_train = true
+        trainstop_control.read_from_train = true
+        trainstop_control.trains_limit_signal = nil
 
-        if debug_log then tools.log(5, 'UpdateStop', 'Circuit deactivated stop: %s', stop.entity.backer_name) end
+        -- skip deactivated stops
+        if trainstop_control.disabled then
+            stop.error_code = 1
+            tools.reduceAvailableCapacity(stop.parked_train_id)
 
-        return
+            setLamp(stop, ErrorCodes[stop.error_code], 1)
+
+            if debug_log then tools.log(5, 'UpdateStop', 'Circuit deactivated stop: %s', stop.entity.backer_name) end
+
+            return
+        end
     end
+
+    local signals = stop.input.get_signals(defines.wire_connector_id.circuit_red, defines.wire_connector_id.circuit_green)
+    if not signals then return end -- either lamp and lampctrl are not connected or lampctrl has no output signal
 
     -- initialize control signal values to defaults
     ---@type ltn.SignalState
@@ -153,9 +165,6 @@ function UpdateStop(stopID, stop)
         locked_slots = 0,
         fuel_signals = nil,
     }
-
-    local signals = stop.input.get_signals(defines.wire_connector_id.circuit_red, defines.wire_connector_id.circuit_green)
-    if not signals then return end -- either lamp and lampctrl are not connected or lampctrl has no output signal
 
     ---@type table<SignalID, number>
     local signals_filtered = {}

script/stop-events.lua
@@ -161,12 +161,15 @@ function CreateStop(entity)
     output.minable = false
     output.destructible = false -- don't bother checking if alive
 
+    -- also fix up the stop entity itself, in case someone meddled with it
+    entity.trains_limit = nil
+
     -- enable reading contents and sending signals to trains
-    local trainstop_control = entity.get_or_create_control_behavior() --[[@as LuaTrainStopControlBehavior]]
-    assert(trainstop_control)
+    local trainstop_control = assert(entity.get_or_create_control_behavior()) --[[@as LuaTrainStopControlBehavior]]
 
     trainstop_control.send_to_train = true
     trainstop_control.read_from_train = true
+    trainstop_control.trains_limit_signal = nil
 
     storage.LogisticTrainStops[entity.unit_number] = {
         entity = entity,

commit d8625b11325d8605ef3bf0f979291aa9e2308e12

UpdateStop: handle limits a little more gracefully
Instead of unilaterally removing station limits from all LTN stations (which
breaks the expected depot behavior) we should remove them when the station type
is Provider, Requester, or Refueler. In the case of Depots, instead either set
the station limit to the value of the LTN Max Trains signal or 1 if that signal
is absent.

Note that I've tested this some but haven't tested it extensively or with other
mods that may add additional LTN station types. Assuming those mods exist and
limit stripping is desired for those new typoes then the method used for
stripping limits from non-Depot stations will need to be revisited.

Fixes: cc51872a823c ("remove any limit on LTN train stops")

script/stop-update.lua
@@ -119,9 +119,6 @@ function UpdateStop(stopID, stop)
         return
     end
 
-    -- also fix up the stop entity itself, in case someone meddled with it
-    stop.entity.trains_limit = nil
-
     local trainstop_control = stop.entity.get_control_behavior() --[[@as LuaTrainStopControlBehavior? ]]
     if trainstop_control then
         trainstop_control.send_to_train = true
@@ -246,6 +243,12 @@ function UpdateStop(stopID, stop)
 
         stop.depot_priority = ltn_state.depot_priority
 
+        if ltn_state.max_trains > 0 then
+            stop.entity.trains_limit = ltn_state.max_trains
+        else
+            stop.entity.trains_limit = 1
+        end
+
         tools.updateStopList(stop, tools.getDepots(), ltn_state.network_id)
 
         -- add parked train to available trains
@@ -271,6 +274,8 @@ function UpdateStop(stopID, stop)
         -- ----------------------------------------------------------------------------------------
         -- Refuel operations
         -- ----------------------------------------------------------------------------------------
+
+        stop.entity.trains_limit = nil
 
         local fuel_prototypes = prototypes.get_item_filtered {
             { filter = 'fuel' },
@@ -305,6 +310,8 @@ function UpdateStop(stopID, stop)
         -- ----------------------------------------------------------------------------------------
         -- Provider / Requester operations
         -- ----------------------------------------------------------------------------------------
+
+        stop.entity.trains_limit = nil
 
         -- check if the name is unique
         tools.reduceAvailableCapacity(stop.parked_train_id)