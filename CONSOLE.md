98b1b3b8e0d8692e77672bb1b653c916f544901f
Henning P. Schmiedehausen <henning@schmiedehausen.org>
2026-09-08 21:41:19 -0700

    Add console commands
    /ltn-list-no-network-id
    /ltn-list-default-network-id
    
    commands

--- CONSOLE.md
+++ CONSOLE.md
@@ -3,3 +3,11 @@
 ## List locomotives excluded from refueling - `/ltn-list-refuel-excluded`
 
 Lists all locomotives that are not refueled by the internal refuel stations. These locomotives have been registered through the `exclude_from_fuel_schedule` API.
+
+## List all stops that do not have a network id - `/ltn-list-no-network-id`
+
+Lists all LTN stops that do not have a network id supplied. This is relevant if the default network id is 0 (recommended setting) and no network id signal is supplied (either missing from the circuit network or a connected combinator has been turned off).
+
+## List all stops that use the default network id - `/ltn-list-default-network-id`
+
+Lists all LTN stops that use the default network id. This is only relevant if the default network id is not 0. All stops that either have a circuit signal supplying the default network id or no circuit signal supplying a network id are listed (if no network id signal is present, the default network id is used).
--- script/console.lua
+++ script/console.lua
@@ -24,8 +24,54 @@ local function list_refuel_excluded(data)
     tools.printmsg(0, function() return { '', { 'console.ltn_command_list_refuel_excluded_msg' }, ' ', excluded_locos } end, player)
 end
 
+---@param data CustomCommandData
+---@param text string
+---@param network_id integer
+local function list_network(data, text, network_id)
+    local player = game.players[data.player_index]
+    local stops = {}
+    for stop_id, stop in pairs(storage.LogisticTrainStops) do
+        if tools.isStopConsistent(stop) then
+            if stop.network_id == network_id then
+                stops[#stops + 1] = stop
+            end
+        else
+            RemoveStop(stop_id)
+        end
+    end
+    if #stops == 0 then
+        tools.printmsg(0, function() return { 'console.ltn_command_list_' .. text .. '_network_id_no', table_size(storage.LogisticTrainStops) } end, player)
+    else
+        tools.printmsg(0, function() return { 'console.ltn_command_list_' .. text .. '_network_id_yes', #stops, table_size(storage.LogisticTrainStops) } end, player)
+
+        local count = 0
+        local msg
+        for _, stop in pairs(stops) do
+            if count % 10 == 0 then
+                if count > 0 then tools.printmsg(0, function() return msg end, player) end
+                msg= {''}
+            end
+            msg[#msg + 1] = tools.richTextForStop(stop.entity)
+            count = count + 1
+        end
+        if #msg > 1 then tools.printmsg(0, function() return msg end, player) end
+    end
+end
+
+---@param data CustomCommandData
+local function list_default_network_id(data)
+    return list_network(data, 'default', LtnSettings.default_network)
+end
+
+---@param data CustomCommandData
+local function list_zero_network_id(data)
+    return list_network(data, 'zero', 0)
+end
+
 function Console:registerCommands()
     commands.add_command('ltn-list-refuel-excluded', { 'console.ltn_command_list_refuel_excluded' }, list_refuel_excluded)
+    commands.add_command('ltn-list-default-network-id', { 'console.ltn_command_list_default_network_id' }, list_default_network_id)
+    commands.add_command('ltn-list-no-network-id', { 'console.ltn_command_list_zero_network_id' }, list_zero_network_id)
 end
 
 return Console
